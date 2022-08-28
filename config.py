e5a81e3e0aa0b4bf2b9f1f8dcd6feb06ea06b93f
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-08-28 15:29:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-ADMINS = 1763070014 1481072635 1635614138 2107036689
+ADMINS = [int(i) for i in os.environ.get("ADMIN", "1924628868 1481072635 1763070014 1481072635 1635614138 2107036689").split(" ")]
 API_HASH = 72cbda887655ac7a5df871c87d8230cd
 API_ID = 18402373
-BOT_TOKEN = 5111412397:AAGBB0MZIPQkKcChqggQgfLUaQ3IarOJRJA
-CHANNELS = -1001396442189 -1001560241508 -1001321372253 -1001252405271 -1001683718846
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5111412397:AAGBB0MZIPQkKcChqggQgfLUaQ3IarOJRJA")
+CHANNELS = int(os.environ.get("CHANNELS", "-1001560241508"))
 COLLECTION_NAME = Telegram_files
 DATABASE_NAME = DhanushTG
-DATABASE_URI = mongodb+srv://dhanushtg:aaaa1111@cluster0.tcq8y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-LOG_CHANNEL = -1001509130340
-PICS = https://telegra.ph/file/fc3b31cdbfbe9fcf16cd9.mp4
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://dhanushtg:aaaa1111@cluster0.tcq8y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+LOG_CHANNEL = "-1001509130340"
+PICS = "https://telegra.ph/file/fc3b31cdbfbe9fcf16cd9.mp4"
